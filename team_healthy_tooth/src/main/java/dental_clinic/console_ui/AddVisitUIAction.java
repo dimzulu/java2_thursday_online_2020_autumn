3dd08883ce5f161e23ee31fb0909ab161717de0b
Alexey777555 <eiroartlv@inbox.lv>
2020-11-23 15:00:24 +0200

    Check patient id (response + request + service) in AddVisit

--- team_healthy_tooth/src/main/java/dental_clinic/console_ui/AddVisitUIAction.java
+++ team_healthy_tooth/src/main/java/dental_clinic/console_ui/AddVisitUIAction.java
@@ -2,8 +2,11 @@ package dental_clinic.console_ui;
 
 import dental_clinic.core.domain.ToothStatus;
 import dental_clinic.core.requests.AddVisitRequest;
+import dental_clinic.core.requests.CheckPatientByIdRequest;
 import dental_clinic.core.responses.AddVisitResponse;
+import dental_clinic.core.responses.CheckPatientByIdResponse;
 import dental_clinic.core.services.AddVisitService;
+import dental_clinic.core.services.CheckPatientByIdService;
 
 import java.util.Optional;
 import java.util.Scanner;
@@ -11,10 +14,12 @@ import java.util.Scanner;
 class AddVisitUIAction implements UIAction {
 
     private AddVisitService addVisitService;
+    private CheckPatientByIdService checkPatientByIdService;
     //InputCheckUtility inputCheckUtility = new InputCheckUtility();
 
-    public AddVisitUIAction(AddVisitService addVisitService) {
+    public AddVisitUIAction(AddVisitService addVisitService, CheckPatientByIdService checkPatientByIdService) {
         this.addVisitService = addVisitService;
+        this.checkPatientByIdService = checkPatientByIdService;
     }
 
     public void execute(){
@@ -23,11 +28,11 @@ class AddVisitUIAction implements UIAction {
         System.out.println("Please enter patient's id");
         long id = in.nextLong();
 
-        AddVisitRequest addVisitRequest = new AddVisitRequest(id,11, Optional.of("comment"),ToothStatus.HEALTHY, "doctor" );
-        AddVisitResponse addVisitResponse = addVisitService.execute(addVisitRequest);
+        CheckPatientByIdRequest checkPatientByIdRequest = new CheckPatientByIdRequest(id);
+        CheckPatientByIdResponse checkPatientByIdResponse = checkPatientByIdService.execute( checkPatientByIdRequest );
 
-        if (addVisitResponse.hasErrors()){
-            System.out.println("Database doesn't contain patient with id ");
+        if (checkPatientByIdResponse.hasErrors()){
+            checkPatientByIdResponse.getErrors().forEach(System.out::println);
         } else {
 
             System.out.println("Please input tooth number");
@@ -47,8 +52,8 @@ class AddVisitUIAction implements UIAction {
             String doctor = in.nextLine();
             doctor = in.nextLine();
 
-            addVisitRequest = new AddVisitRequest(id, toothNumber, comment, toothStatus, doctor);
-            addVisitResponse = addVisitService.execute(addVisitRequest);
+            AddVisitRequest addVisitRequest = new AddVisitRequest(id, toothNumber, comment, toothStatus, doctor);
+            AddVisitResponse addVisitResponse = addVisitService.execute(addVisitRequest);
 
             if (addVisitResponse.hasErrors()) {
                 addVisitResponse.getErrors().forEach(System.out::println);
