fa92fce1d65085a8089dbb9c513ec12b0c5973a5
ArturM <kpackalv@yahoo.com>
2020-11-25 21:59:25 +0200

    StaticStartup L4 Paging+Ordering FindProductsRequestValidator

--- team_static_startup/src/main/java/internet_store/application/core/services/FindProductsService.java
+++ team_static_startup/src/main/java/internet_store/application/core/services/FindProductsService.java
@@ -4,11 +4,14 @@ import internet_store.application.core.database.Database;
 import internet_store.application.core.domain.Product;
 import internet_store.application.core.requests.FindProductsRequest;
 import internet_store.application.core.requests.Ordering;
+import internet_store.application.core.requests.Paging;
 import internet_store.application.core.responses.CoreError;
 import internet_store.application.core.responses.FindProductsResponse;
 import internet_store.application.core.services.validators.FindProductsRequestValidator;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FindProductsService {
 
@@ -29,8 +32,21 @@ public class FindProductsService {
 
         List<Product> products = search(request);
         products = order(products, request.getOrdering());
+        products = paging(products, request.getPaging());
 
         return new FindProductsResponse(products, null);
+    }
+
+    private List<Product> paging(List<Product> products, Paging paging) {
+        if (paging != null) {
+            int skip = (paging.getPageNumber() - 1) * paging.getPageSize();
+            return products.stream()
+                    .skip(skip)
+                    .limit(paging.getPageSize())
+                    .collect(Collectors.toList());
+        } else {
+            return products;
+        }
     }
 
     private List<Product> search(FindProductsRequest request) {
@@ -48,7 +64,17 @@ public class FindProductsService {
     }
 
     private List<Product> order(List<Product> productList, Ordering ordering){
-        return productList;
+        if (ordering != null) {
+            Comparator<Product> comparator = ordering.getOrderBy().equals("Name")
+                    ? Comparator.comparing(Product::getName)
+                    : Comparator.comparing(Product::getDescription);
+            if (ordering.getOrderDirection().equals("DESCENDING")) {
+                comparator = comparator.reversed();
+            }
+            return productList.stream().sorted(comparator).collect(Collectors.toList());
+        } else {
+            return productList;
+        }
     }
 
 }
